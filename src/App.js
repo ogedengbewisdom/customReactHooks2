b641d080e8b431bd35b175da2e79231b7dc92c87
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-03-25 04:50:56 +0100

    Using http custom hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
 import useHttpRequest from './hooks/useHttp';
@@ -7,26 +7,25 @@ import useHttpRequest from './hooks/useHttp';
 function App() {
   const [tasks, setTasks] = useState([]);
 
-  const httpObjVal = useMemo( () => {
-    return {
-    url: 'https://customreacthooks-ce486-default-rtdb.firebaseio.com/tasks.json'
-  }}, [])
-  const httpDataFunc = useCallback((data) => {
-  
-    const loadedTasks = []
-    for (const taskKey in data) {
-      loadedTasks.push({
-        id: taskKey,
-        text: data[taskKey].text
-      })
-    }
-    setTasks(loadedTasks)
-  }, [])
-
-  const {isLoading, error, sendHttpRequest: fetchTasks} = useHttpRequest(httpObjVal, httpDataFunc)
+  const {isLoading, error, sendHttpRequest: fetchTasks} = useHttpRequest()
 
   useEffect(() => {
-    fetchTasks();
+    const httpDataFunc = (data) => {
+  
+      const loadedTasks = []
+      for (const taskKey in data) {
+        loadedTasks.push({
+          key: taskKey,
+          id: taskKey,
+          text: data[taskKey].text
+        })
+      }
+      setTasks(loadedTasks)
+    }
+    const httpObjVal = {
+      url: 'https://customreacthooks-ce486-default-rtdb.firebaseio.com/tasks.json'
+    }
+    fetchTasks(httpObjVal, httpDataFunc);
   }, [fetchTasks]);
 
 
